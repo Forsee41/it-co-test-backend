ca7eea7b7a50e74f0784368342557def16ae64a1
Forsee41 <forsee41@gmail.com>
2023-11-20 07:48:45 +0300

    add dal patch tests

--- tests/test_dal.py
+++ tests/test_dal.py
@@ -2,7 +2,12 @@ from contextlib import asynccontextmanager
 
 import pytest
 
-from it_co_test.db.dal import add_project, delete_project, get_all_projects
+from it_co_test.db.dal import (
+    add_project,
+    delete_project,
+    get_all_projects,
+    patch_project,
+)
 from it_co_test.db.session import session
 
 
@@ -55,6 +60,48 @@ async def test_delete_project() -> None:
         assert len(projects) == 1
         remaining = projects[0]
         assert remaining.name == project2["name"]
+
+
+async def test_patch_all_fields() -> None:
+    project1 = {"image": "image1", "description": "description1", "name": "name1"}
+    project1_changed = {
+        "image": "image3",
+        "description": "description3",
+        "name": "name3",
+    }
+    project2 = {"image": "image2", "description": "description2", "name": "name2"}
+    db = asynccontextmanager(session)
+    async with db() as sess:
+        assert sess
+        added1 = await add_project(sess, **project1)
+        await add_project(sess, **project2)
+        patch_returned = await patch_project(sess, id=added1.id, **project1_changed)
+        queried = await get_all_projects(sess)
+        patched = queried[0] if queried[0].id == added1.id else queried[1]
+        non_patched = queried[0] if patched == queried[1] else queried[1]
+        assert patch_returned.image == patched.image
+        assert patch_returned.name == patched.name
+        assert patch_returned.description == patched.description
+        assert patch_returned.image == project1_changed["image"]
+        assert patch_returned.name == project1_changed["name"]
+        assert patch_returned.description == project1_changed["description"]
+        assert non_patched.name == project2["name"]
+
+
+async def test_patch_single_field() -> None:
+    project = {"image": "image1", "description": "description1", "name": "name1"}
+    db = asynccontextmanager(session)
+    async with db() as sess:
+        assert sess
+        added = await add_project(sess, **project)
+        patch_returned = await patch_project(sess, id=added.id, name="new_name")
+        projects = await get_all_projects(sess)
+        assert projects
+        patched = projects[0]
+        assert patch_returned.name == "new_name"
+        assert patched.name == "new_name"
+        assert patch_returned.image == project["image"]
+        assert patched.image == project["image"]
 
 
 if __name__ == "__main__":
