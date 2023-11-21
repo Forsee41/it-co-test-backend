4f8102657d5b425979a1475db21761c484e1ac3d
Forsee41 <forsee41@gmail.com>
2023-11-21 18:51:43 +0300

    add cors

--- it_co_test/app.py
+++ it_co_test/app.py
@@ -1,10 +1,21 @@
 from fastapi import APIRouter, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from it_co_test.api.routers import project, service
+
+app = FastAPI(title="IT-CO-Test")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 root_router = APIRouter()
 root_router.include_router(service.router, tags=["Service"])
 root_router.include_router(project.router, tags=["Projects"])
 
-app = FastAPI(title="IT-CO-Test")
 app.include_router(root_router)
