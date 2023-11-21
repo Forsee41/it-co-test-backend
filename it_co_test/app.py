05421326d6160eaf9636f9bb3f081d040abb5935
Forsee41 <forsee41@gmail.com>
2023-11-21 15:28:50 +0300

    add app init

--- it_co_test/app.py
+++ it_co_test/app.py
@@ -1,3 +1,9 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
+
+from it_co_test.api.routers import service
+
+root_router = APIRouter()
+root_router.include_router(service.router, tags=["Service"])
 
 app = FastAPI(title="IT-CO-Test")
+app.include_router(root_router)
