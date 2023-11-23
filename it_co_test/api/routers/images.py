c315149df0799a41473d92d34d4e5999f49ca726
Forsee41 <forsee41@gmail.com>
2023-11-23 21:56:53 +0300

    add image deletion

--- it_co_test/api/routers/images.py
+++ it_co_test/api/routers/images.py
@@ -9,11 +9,17 @@ from fastapi import APIRouter, Depends, File, HTTPException, UploadFile
 from fastapi.responses import FileResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from it_co_test.db.dal import ProjectNotFound, register_image
+from it_co_test.db.dal import ProjectNotFound, patch_project, register_image
 from it_co_test.db.session import session
 
 router = APIRouter()
 DBDependency = Annotated[AsyncSession, Depends(session)]
+
+
+async def delete_images_by_id(id: UUID) -> None:
+    filenames = glob.glob(f"images/{id}*")
+    for filename in filenames:
+        os.remove(filename)
 
 
 @router.put("/{id}")
@@ -32,9 +38,20 @@ async def put_image(
         except ProjectNotFound:
             raise HTTPException(status_code=404, detail="Project not found")
 
+    await delete_images_by_id(id)
     async with aiofiles.open(target_filename, "wb") as out_file:
         while content := await file.read(1024):
             await out_file.write(content)
+
+
+@router.put("/{id}")
+async def delete_image(session: DBDependency, id: UUID) -> None:
+    await delete_images_by_id(id)
+    async with session:
+        try:
+            await patch_project(session, id=id, image=False)
+        except ProjectNotFound:
+            raise HTTPException(status_code=404, detail="Project not found")
 
 
 @router.get("/{id}")
